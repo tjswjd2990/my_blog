960a04ee7bbe3c9959e7fdeadb03e635493616fe
tjswjd2990 <tjswjd2990@hanmail.net>
2020-07-03 18:48:43 +0900

    php더공부하기

--- public/MY_BLOG_test.php
+++ public/MY_BLOG_test.php
@@ -1,35 +1,43 @@
 <?php
 $dbConn = mysqli_connect("site11.blog.oa.gg", "site11", "sbs123414", "site11", 3306) or die("DB CONNECTION ERROR");
 
-$cateItemId = $_GET['cateItemId'];
+$cateItemId = $_GET['cateItemId'];  /*주소창에서 물음표 뒤에 cateItemId=1을 붙이면 1을 가져온다*/
+/*$_GET은 cateItemId에 입력한 값에 따라 각각 다른 결과를 보여주는 역할을 해줌*/
 
 $sql = "
 SELECT *
 FROM cateItem
 WHERE id = {$cateItemId}
 ";
-$rs = mysqli_query($dbConn, $sql);
+/*{$cateItemId} 자리에 그냥 1이나 2를 넣으면 1이랑 2밖에 안나오기 때문에 다양한 값이 들어갈 수 있도록 {$cateItemId}라고 입력해 뭉뚱그려 사용*/
+
+$rs = mysqli_query($dbConn, $sql);  /*$dbConn과 $sql을 가져와서 실행시킨 결과를 압축시키는 것*/
 $row = mysqli_fetch_assoc($rs);
 $cateItemName = $row['name'];
 
-$sql = "
+$sql2 = "
 SELECT *
 FROM article
 WHERE cateItemId = {$cateItemId}
-ORDER BY ID DESC
+ORDER BY id DESC
 ";
-$rs = mysqli_query($dbConn, $sql);
-$articleRows = [];
-while ( true ) {
-    $row = mysqli_fetch_assoc($rs);
 
-    if ( $row == null ) {
+$rs2 = mysqli_query($dbConn, $sql2);
+
+$articleRows = [];
+
+while ( true ) {
+    $row2 = mysqli_fetch_assoc($rs2);
+
+    if ( $row2 == null ) {
         break;
     }
 
-    $articleRows[] = $row;
+    $articleRows[] = $row2;
 }
+
 ?>
+
 <h1>카테고리 : <?=$cateItemName?></h1>
 <?php foreach ( $articleRows as $article ) { ?>
 <div>
