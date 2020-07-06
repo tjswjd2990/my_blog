35c6d443dd444894f0bc766d3724bb1047d24a27
tjswjd2990 <tjswjd2990@hanmail.net>
2020-07-06 18:54:41 +0900

    메인이미지진행중

--- part/foot.php
+++ part/foot.php
@@ -1,33 +1,4 @@
-<!--
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
-    <link rel="stylesheet" href="/resource/common.css">
-    <link rel="icon" href="/resource/images/favicon.png">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="/resource/common.js"></script>
-<div class="foot-bar flex flex-ai-c flex-jc-c">
-    <div class="foot-abata">
-        <a href="#"><img src="/resource/images/abata.png" alt=""></a>
-    </div>
-    <nav class="menu-box-1">
-        <ul class="flex flex-jc-c">
-        <li><a href="#">Call</a>
-            <ul class="flex flex-jc-c">
-                <li><a href="#">010-4123-2990</a></li>
-            </ul>
-    </li>
-        <li><a href="#">Follow</a>
-            <ul class="flex flex-jc-c">
-                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                <li><a href="#"><img src="/resource/images/tistoryicon.png" alt=""></a></li>
-                <li><a href="#"><i class="fab fa-github"></i></a></li>
-                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-            </ul>
-        </li>
-        <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
-</div>
--->
+
 </body>
 </html>
 
--- public/index.php
+++ public/index.php
@@ -1,7 +1,16 @@
 <?php 
 include_once "../part/head.php"
 ?>
-<h1 class="con">home</h1>
+    <div class="bbox con flex">
+        <div class="sns-list">
+            <a href="https://www.youtube.com/channel/UCANwkCXCfogf2_veRAv3V3g?view_as=subscriber" target="_blank">Youtube</a>
+            <a href="https://am-jei.tistory.com/" target="_blank">Tistory</a>
+            <a href="https://github.com/tjswjd2990" target="_blank">Github</a>
+            <a href="https://www.instagram.com/am_jei_/" target="_blank">Instagram</a>
+        </div>
+        <div class=box1></div>
+        <div class=box2></div>
+    </div>
 <?php 
 include_once "../part/foot.php"
 ?>
