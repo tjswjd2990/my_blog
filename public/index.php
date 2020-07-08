671bb1b10186b216a40340a47b7b18cfd2ec1c18
tjswjd2990 <tjswjd2990@hanmail.net>
2020-07-08 14:33:24 +0900

    탑바글씨체바꾸고sns호버효과

--- public/index.php
+++ public/index.php
@@ -2,13 +2,15 @@
 include_once "../part/head.php"
 ?>
     <div class="bbox con flex">
-        <div class="sns-list">
-            <a href="https://www.youtube.com/channel/UCANwkCXCfogf2_veRAv3V3g?view_as=subscriber" target="_blank">Youtube</a>
-            <a href="https://am-jei.tistory.com/" target="_blank">Tistory</a>
-            <a href="https://github.com/tjswjd2990" target="_blank">Github</a>
-            <a href="https://www.instagram.com/am_jei_/" target="_blank">Instagram</a>
+        <ul class="sns-list flex">
+            <li><a href="https://www.youtube.com/channel/UCANwkCXCfogf2_veRAv3V3g?view_as=subscriber" target="_blank">Youtube</a></li>
+            <li><a href="https://am-jei.tistory.com/" target="_blank">Tistory</a></li>
+            <li><a href="https://github.com/tjswjd2990" target="_blank">Github</a></li>
+            <li><a href="https://www.instagram.com/am_jei_/" target="_blank">Instagram</a></li>
+        </ul>
+        <div class=box1>
+            MIN
         </div>
-        <div class=box1></div>
         <div class=box2></div>
     </div>
 <?php 
