cd7e8ddaaff37419a973ad544ff2461621b0f318
tjswjd2990 <tjswjd2990@hanmail.net>
2020-07-13 18:50:00 +0900

    PUBG작업시작

---
+++ public/pf/PUBG/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="index.js"></script>
+
+    <title>PUBG</title>
+</head>
+<body>
+<div class="top-bar">
+<div class="logo-box">
+<div class="logo">
+<img src="https://d33wubrfki0l68.cloudfront.net/cc2f9575c7accaa319393b433924b510ff64c474/408bc/static/assets/img/global-pcs-logo.png" alt="">
+</div>
+</div>
+<div>2</div>
+<div>3</div>
+</div>
+</body>
+</html>
