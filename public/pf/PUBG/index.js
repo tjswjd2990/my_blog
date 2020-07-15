1e8c0668b6c1e283a5885d58d4b4f6572113ceae
tjswjd2990 <tjswjd2990@hanmail.net>
2020-07-15 18:53:42 +0900

    작업중

---
+++ public/pf/PUBG/index.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.img-slider > .owl-carousel').owlCarousel({
+        items: 1,
+        nav: true,
+        navText: [
+        '<img src="https://tjswjd2990.github.io/img1/pf/PUBG/ico_slick_button.png" alt="">',
+        '<img src="https://tjswjd2990.github.io/img1/pf/PUBG/ico_slick_button.png" alt="">'
+        ],
+        navContainer: '.img-slider > .nav-icon',
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true
+    });
+})
--- public/pf/PUBG/index.php
+++ public/pf/PUBG/index.php
@@ -4,29 +4,81 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
-        rel="stylesheet">
     <link rel="stylesheet" href="index.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
     <script src="index.js"></script>
+
 
     <title>PUBG</title>
 </head>
 <body>
-<!--
-<div class="top-bar">
-    <div class="logo-box">
-        <div class="logo">
-            <img src="https://d33wubrfki0l68.cloudfront.net/cc2f9575c7accaa319393b433924b510ff64c474/408bc/static/assets/img/global-pcs-logo.png" alt="">
+<!--탑바 시작-->
+<div class="top-bar con flex">
+    <div class="logo img-box">
+        <img src="https://tjswjd2990.github.io/img1/pf/PUBG/global-pcs-logo.png" alt="">
+    </div>
+    <nav class="menu-box-1 flex-grow-1">
+        <ul class="flex">
+            <li><a class="block" href="#">NEWS</a></li>
+            <li><a class="block" href="#">TEAM</a></li>
+            <li><a class="block" href="#">SHEDULE</a></li>
+            <li><a class="block" href="#">RANKING</a></li>
+        </ul>
+    </nav>
+    <div class="lan-icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="31.951" height="31.951" viewBox="0 0 31.951 31.951"><path id="Color_Overlay" data-name="Color Overlay" d="M721.4,269.975a15.992,15.992,0,0,1-15.956-15.287h-.017v-1.375h.017A15.974,15.974,0,1,1,721.4,269.975h0Zm.689-1.444c2.063-.4,3.87-2.609,5.041-5.838a26.977,26.977,0,0,0-5.041-.585Zm-6.416-5.838c1.171,3.229,2.976,5.433,5.04,5.838v-6.423A26.949,26.949,0,0,0,715.672,262.692Zm12.007,2.235a11.979,11.979,0,0,1-2.061,3.051,14.591,14.591,0,0,0,6.134-3.685,17.24,17.24,0,0,0-3.289-1.272A18.183,18.183,0,0,1,727.679,264.927Zm-16.63-.634a14.583,14.583,0,0,0,6.133,3.685,11.954,11.954,0,0,1-2.06-3.051,18.385,18.385,0,0,1-.785-1.905A17.23,17.23,0,0,0,711.049,264.293Zm17.838-2.6a17.791,17.791,0,0,1,3.82,1.539,14.529,14.529,0,0,0,3.274-8.546h-6.092A28.047,28.047,0,0,1,728.887,261.694Zm-18.795,1.538a17.736,17.736,0,0,1,3.821-1.538,28.089,28.089,0,0,1-1-7.007h-6.092A14.533,14.533,0,0,0,710.092,263.232Zm12-2.5a28.525,28.525,0,0,1,5.468.629,26.692,26.692,0,0,0,.965-6.673h-6.433Zm-6.844.629a28.445,28.445,0,0,1,5.468-.629v-6.043h-6.434A26.709,26.709,0,0,0,715.244,261.36Zm13.644-15.053a28.081,28.081,0,0,1,1,7h6.092a14.538,14.538,0,0,0-3.272-8.544A17.728,17.728,0,0,1,728.888,246.308Zm-6.8.962v6.043h6.433a26.719,26.719,0,0,0-.965-6.674A28.449,28.449,0,0,1,722.088,247.27Zm-7.81,6.043h6.434V247.27a28.531,28.531,0,0,1-5.468-.631A26.71,26.71,0,0,0,714.278,253.312Zm-7.459,0h6.092a28.124,28.124,0,0,1,1-7,17.751,17.751,0,0,1-3.823-1.535A14.531,14.531,0,0,0,706.819,253.312Zm15.269-7.419a26.956,26.956,0,0,0,5.04-.587c-1.171-3.229-2.977-5.432-5.04-5.837Zm-6.416-.587a27.022,27.022,0,0,0,5.04.587V239.47C718.648,239.874,716.843,242.077,715.672,245.307Zm12.007-2.234a18.512,18.512,0,0,1,.783,1.9,17.193,17.193,0,0,0,3.29-1.269,14.593,14.593,0,0,0-6.134-3.686A11.979,11.979,0,0,1,727.679,243.073Zm-16.631.635a17.29,17.29,0,0,0,3.289,1.269,18.639,18.639,0,0,1,.785-1.9,11.954,11.954,0,0,1,2.06-3.051A14.593,14.593,0,0,0,711.048,243.707Z" transform="translate(-705.424 -238.025)" fill="#fff"></path></svg>
+    </div>
+</div>
+<!--탑바 끝-->
+
+<!--상단 이미지 슬라이더 시작-->
+
+<!--슬라이더 이미지들-->
+<div class="img-slider">
+    <nav class="nav-icon"></nav>
+    <div class="owl-carousel owl-theme">
+        <div class="item" style="background-image:url(https://tjswjd2990.github.io/img1/pf/PUBG/200608_main_PC_APAC-2-1500x797.png);">
+            <div class="img-text-box">
+                <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                <p class="area">APAC</p>
+                <a href="#" class="more">VIEW MORE</a>
+            </div>
+        </div>
+        <div class="item" style="background-image:url(https://tjswjd2990.github.io/img1/pf/PUBG/200526_main_PC_01-1500x797.png);">
+            <div class="img-text-box">
+                <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                <p class="area">PUGB Esports Player Stats+(PEPS+)</p>
+                <a href="#" class="more">VIEW MORE</a>
+            </div>
+        </div>
+        <div class="item" style="background-image:url(https://tjswjd2990.github.io/img1/pf/PUBG/200608_main_PC_EU-2-1500x797.png);">
+            <div class="img-text-box">
+                <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                <p class="area">EUROPE</p>
+                <a href="#" class="more">VIEW MORE</a>
+            </div>
+        </div>
+        <div class="item" style="background-image:url(https://tjswjd2990.github.io/img1/pf/PUBG/200608_main_PC_NA-2-1500x797.png);">
+            <div class="img-text-box">
+                <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                <p class="area">NORTH AMERICA</p>
+                <a href="#" class="more">VIEW MORE</a>
+            </div>
+        </div>
+        <div class="item" style="background-image:url(https://tjswjd2990.github.io/img1/pf/PUBG/200608_main_PC_ASIA-2-1500x797.png);">
+            <div class="img-text-box">
+                <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                <p class="area">ASIA</p>
+                <a href="#" class="more">VIEW MORE</a>
+            </div>
         </div>
     </div>
-<div>2</div>
-<div>3</div>
 </div>
--->
-<div class="img-slide">
-</div>
+<!--상단 이미지 슬라이더 끝-->
 </body>
 </html>
