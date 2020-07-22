450020247022c00bb08122852e7e2bee1732f70d
tjswj2990 <tjswj2990@hanmail.net>
2020-07-22 18:49:31 +0900

    이지웰하다가말았음

--- public/pf/ezwell/index.js
+++ public/pf/ezwell/index.js
@@ -1,11 +1,18 @@
-var mySwiper = new Swiper('.swiper-container', {
-  navigation: {
-    prevEl: '.swiper-button-prev',
-    nextEl: '.swiper-button-next',
-  },
-  slidesPerView: 1,
-  loop: true,
-  autoplay: {
-    delay: 2000,
-  }
-});
+function Siler_1(){
+  var mySwiper = new Swiper('.swiper-container', {
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+    },
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    }
+  });
+}
+$(function(){
+  Siler_1();
+})
+
+
