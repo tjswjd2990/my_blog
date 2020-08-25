3be7e3b65daddff47cd431ef4e4825e2a38b438b
tjswjd2990 <tjswjd2990@hanmail.net>
2020-08-25 18:41:41 +0900

    넷플릭스리뉴얼두번째

--- public/pf/netflix_re/index.php
+++ public/pf/netflix_re/index.php
@@ -1,47 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    
+
     <script src="https://alvarotrigo.com/fullPage/fullpage.js"></script>
     <link rel="stylesheet" href="https://alvarotrigo.com/fullPage/fullpage.css">
 
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-    
-    <link rel="stylesheet" href="./index.css">
-    <script src="./index.js"></script>
+
+    <link rel="stylesheet" href="/pf/netflix_re/index.css">
+    <script src="/pf/netflix_re/index.js"></script>
 
     <title>netfilx_re</title>
 </head>
-<body>
-<div id="fullpage">
-    <!--첫번째 페이지 시작-->
-  <div class="section main">
-    <div class="video-box">
-        <video oncontextmenu="return false;" id="myVideo" autoplay muted loop>
-            <source src="https://tjswjd2990.github.io/img1/pf/netflix_re/main_video.mp4" type="video/mp4">
-        </video>
-    </div>
-    <div class="login_box flex">
-        <a href="#" class="login">Login</a>
-        <a href="#" class="membership">멤버십 가입</a>
-        <a href="#" class="service">고객센터</a>
-    </div>
-    <div class="btm-box"></div>
-    <div class="scroll_down flex">
-        <div class="line"></div>
-        <div class="scroll_text">Scroll Down</div>
-    </div>
-  </div>
-  <!--첫번째 페이지 끝-->
-  <div class="section">내용2</div>
-  <div class="section">내용3</div>
-  <div class="section">내용4</div>
-</div> 
 
+<body>
+    <div id="fullpage">
+        <!--첫번째 페이지 시작-->
+        <div class="section first">
+            <div class="video-box">
+                <video oncontextmenu="return false;" id="myVideo" autoplay muted loop>
+                    <source src="https://tjswjd2990.github.io/img1/pf/netflix_re/main_video.mp4" type="video/mp4">
+                </video>
+            </div>
+            <div class="main_text">
+                <div class="unlimited">영화, TV 프로그램을 무제한으로!</div>
+                <img class="main_logo" src="https://tjswjd2990.github.io/img1/pf/netflix_re/netflix_logo.png">
+                <a href="#" class="block free">Free start for 30 days →</a>
+            </div>
+            <div class="login_box flex">
+                <a href="#" class="login">Login</a>
+                <a href="#" class="membership">멤버십 가입</a>
+                <a href="#" class="service">고객센터</a>
+            </div>
+            <div class="btm-box"></div>
+            <div class="scroll_down flex">
+                <div class="line"></div>
+                <div class="scroll_text">Scroll Down</div>
+            </div>
+        </div>
+        <!--첫번째 페이지 끝-->
+        <!--두번째 페이지 시작-->
+        <div class="section second">
+            <img src="https://tjswjd2990.github.io/img1/pf/netflix_re/device.png" class="device">
+            <div class="device-video">
+                <video oncontextmenu="return false;" id="myVideo" autoplay muted loop>
+                    <source src="https://tjswjd2990.github.io/img1/pf/netflix_re/device.mp4" type="video/mp4">
+                </video>
+            </div>
+            <div class="second-main-text">
+                <div class="one">
+                    <div class="first-text">다양한 디바이스에서</div>
+                    <div class="second-text">마음껏 즐길 수 있는 자유</div>
+                </div>
+                <div class="two">
+                    <div class="third-text">각종 영화와 TV 프로그램을 스마트폰, 태블릿,</div>
+                    <div class="forth-text">노트북, TV에서 추가요금 없이 무제한 스트리밍!</div>
+                </div>
+                <a href="#" class="small-free">Free start for 30 days →</a>
+            </div>
+            <div class="login_box flex">
+                <a href="#" class="login">Login</a>
+                <a href="#" class="membership">멤버십 가입</a>
+                <a href="#" class="service">고객센터</a>
+            </div>
+            <div class="btm-box"></div>
+            <div class="scroll_down flex">
+                <div class="line"></div>
+                <div class="scroll_text">Scroll Down</div>
+            </div>
+            <div class="nlogo img-box">
+                <img src="http://tjswjd2990.github.io/img1/pf/netflix_re/n.png" alt="">
+            </div>
+        </div>
+    <!--두번째 페이지 끝-->
+        <div class="section">내용3</div>
+        <div class="section">내용4</div>
+    </div>
 </body>
+
 </html>
