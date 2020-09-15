951a3c694f05b213df6ea42e8338a5bce1a71119
tjswjd2990 <tjswjd2990@hanmail.net>
2020-09-15 18:32:11 +0900

    다시저장

--- public/pf/index.php
+++ public/pf/index.php
@@ -23,10 +23,10 @@
             <a href="/pf/netflix_re">넷플릭스 리뉴얼</a>
         </li>
         <li>
-            <a href="pf/design/main.png">포트폴리오메인시안</a>
+            <a href="/pf/design/main.png">포트폴리오메인시안</a>
         </li>
         <li>
-            <a href="pf/design/about.png">포트폴리오어바웃미시안</a>
+            <a href="/pf/design/about.png">포트폴리오어바웃미시안</a>
         </li>
     </ul>
 </body>
