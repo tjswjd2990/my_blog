669e0d04d7d867104e6121e6cc977becedfdaf11
tjswjd2990 <tjswjd2990@hanmail.net>
2020-09-24 18:37:28 +0900

    포트폴리오사이트시작

---
+++ public/pf/portfolio_site/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="/pf/portfolio_site/index.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/pf/portfolio_site/index.js"></script>
+
+    <title>portfolio_site</title>
+</head>
+<body>
+    <div class="top-bar">
+        <div class="logo">
+            
+        </div>
+        <div class="menu-box-1">
+            <ul class="flex">
+                <li><a href="#">Main</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>        
+        </div>
+    </div>
+    <div class="main">
+        <div class="main-img"></div>
+        <div class="main-txt">
+            <div class="web-designer">Web Designer</div>
+            <div class="name">Kim min jeong</div>
+        </div>
+    </div>
+
+</body>
+</html>
